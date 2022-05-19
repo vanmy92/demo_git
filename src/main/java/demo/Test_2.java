da1d61ee4fe68395ce02ed4ee194576b96f62e7f
my.nguyen <vanmy9299@gmail.com>
2022-05-19 10:02:26 +0700

    demo congty lan 2

--- src/main/java/demo/Test_2.java
+++ src/main/java/demo/Test_2.java
@@ -2,6 +2,8 @@ package demo;
 
 public class Test_2 {
     public static void main(String[] args) {
-        System.out.println("haadsfasdf");
+        System.out.println("asgaadsfadssdgasdg");
+        System.out.println("asdfads");
+
     }
 }
